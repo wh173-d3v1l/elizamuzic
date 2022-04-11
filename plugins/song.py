f7fe31a3d3e09a5ab12d8339f150e0ca4451ee20
Mohamed Rizad <rizadvillan@gmail.com>
2022-04-11 21:48:48 +0530

    Update song.py

--- plugins/song.py
+++ plugins/song.py
@@ -100,7 +100,7 @@ def a(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep =  f'🎧 ᴛɪᴛᴛʟᴇ : {title[:35]}\n ᴅᴜʀᴀᴛɪᴏɴ : {duration}\n ᴠɪᴇᴡs : {views}\n\nTo : {message.from_user.mention()}\n Queries : [@shubaiba](https://instagram.com/_shubaiba__)'
+        rep =  f'🎧 ᴛɪᴛᴛʟᴇ : {title[:35]}\n ᴅᴜʀᴀᴛɪᴏɴ : {duration}\n ᴠɪᴇᴡs : {views}\n\nTo : {message.from_user.mention()}\n Queries : @shubaiba'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
